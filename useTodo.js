20a01395a245b23b251a67d5bc173fe98937269a
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2023-11-27 16:43:57 -0700

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,55 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+
+const init = () => {
+  return JSON.parse( localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+ 
+    const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+      localStorage.setItem( 'todos', JSON.stringify( todos ) );
+    
+    }, [todos])
+    
+  
+    const handleNewTodo = ( todo ) => {
+      const action = {
+        type: '[TODO] Add Todo',
+        payload: todo
+      }
+      dispatch( action );
+    }
+  
+    const handleDeleteTodo = ( id ) => {
+    //  console.log( {id} )
+      dispatch ({
+        type: '[TODO] Delete Todo',
+        payload: id
+      })
+    }
+  
+    const handleToggleTodo = ( id ) => {
+      // console.log( {id} );
+      dispatch ({
+        type: '[TODO] Toggle Todo',
+        payload: id
+      })
+    }
+
+    return {
+      todos,
+
+      todosCount: todos.length,
+      pendingTodoCount: todos.filter(todo => !todo.done).length,
+
+      handleNewTodo,
+      handleDeleteTodo,
+      handleToggleTodo
+    }
+    
+}
